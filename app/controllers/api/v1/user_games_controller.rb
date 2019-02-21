7f6ce7e8c688e66f163bb44b86f8b7f6b1f42bcf
lumrachele <rlum894@gmail.com>
2019-02-21 15:48:50 -0500

    upload with cloudinary and update with url in db

--- app/controllers/api/v1/user_games_controller.rb
+++ app/controllers/api/v1/user_games_controller.rb
@@ -1,4 +1,12 @@
+require "base64"
 class Api::V1::UserGamesController < ApplicationController
+
+  before_action :check_configuration
+
+  def check_configuration
+    render 'configuration_missing' if Cloudinary.config.api_key.blank?
+  end
+
   def index
     @userGames = UserGame.all
     render json: @userGames
@@ -20,6 +28,23 @@ class Api::V1::UserGamesController < ApplicationController
     render json: @userGame
   end
 
+  def edit
+    @userGame = UserGame.find(params[:id])
+    render json: @userGame
+  end
+
+  def update
+    @userGame = UserGame.find(params[:id])
+    # @userGame.update(user_game_params)
+    # byebug
+    getURL = uploadToCloudinary(user_game_params[:imageUrl])
+    @userGame.update_attribute(:imageUrl, getURL)
+    # byebug
+    render json: @userGame, status: :ok
+  end
+
+
+
   def destroy
     @userGame = UserGame.find(params[:id])
     @userGame.destroy
@@ -30,6 +55,11 @@ class Api::V1::UserGamesController < ApplicationController
 
   def user_game_params
     params.require(:user_game).permit(:id, :user_id, :game_id, :imageUrl)
+  end
+
+
+  def uploadToCloudinary(data)
+    Cloudinary::Uploader.upload(data)["url"]
   end
 
 
